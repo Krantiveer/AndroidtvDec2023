e5e8110d949d57035bece8edfb5e268899b81022
ubuntu <krantiveer.singh@phando.com>
2023-01-19 20:06:35 +0530

    launch code

--- app/src/main/java/com/ott/tv/Config.java
+++ app/src/main/java/com/ott/tv/Config.java
@@ -7,39 +7,31 @@ public class Config {
 //    public static final String DOMAIN = "https://uvtvstage.phando.com/";
 
 
+/*
     public static final String DOMAIN = "https://uvtvstage.phando.com/";
     public static final String Publisher_id = "4";
     public static final String WebsiteURL = "https://uvtv.phando.com";
 
+*/
 
-   /* public static final String DOMAIN = "https://kaafaltv.com/";
+  /*  public static final String DOMAIN = "https://kaafaltv.com/";
     public static final String Publisher_id = "5";
     public static final String WebsiteURL = "https://kaafaltv.com";
 */
-
-/*
     public static final String DOMAIN = "https://www.solidtvbox.com/";
     public static final String Publisher_id = "6";
     public static final String WebsiteURL = "https://www.solidtvbox.com/";
 
-*/
 /*
-
     public static final String DOMAIN = "https://saas-sandbox.phando.com/";
     public static final String Publisher_id = "28";
-    public static final String WebsiteURL = "https://saas-sandbox.phando.com/";
-*/
-
+    public static final String WebsiteURL = "https://saas-sandbox.phando.com/";*/
     public static final String API_SERVER_URL = DOMAIN + "api/";
-
     //copy your api key from php admin dashboard & paste below
     public static final String API_KEY = "75xi3uer76tb7krer3mjgqei";
-
     public static final String Device_Type = "android";
-
     //Phone authentication
     public static final boolean ENABLE_PHONE_LOGIN = true;
-
     //Google authentication
     public static final boolean ENABLE_GOOGLE_LOGIN = false;
 
