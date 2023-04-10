ee1ef2c2061d87e73a79b0d2306b3c6bc6c2dacc
ubuntu <krantiveer.singh@phando.com>
2023-04-10 13:09:44 +0530

    create seperated icon

--- app/src/main/java/com/ott/tv/Config.java
+++ app/src/main/java/com/ott/tv/Config.java
@@ -9,9 +9,11 @@ public class Config {
     //public static final String DOMAIN = "https://lifeott.com/";
 //    public static final String DOMAIN = "https://uvtvstage.phando.com/";
 
+/*
     public static final String DOMAIN = "https://www.uvtv.in/";
     public static final String Publisher_id = "4";
     public static final String WebsiteURL = "https://www.uvtv.in/";
+*/
 
 
     /*  public static final String DOMAIN = "https://kaafaltv.com/";
@@ -21,15 +23,14 @@ public class Config {
     /*public static final String DOMAIN = "https://ott.digiana.com/";
     public static final String Publisher_id = "63";
     public static final String WebsiteURL = "https://ott.digiana.com/";*/
-
- /*   public static final String DOMAIN = "https://www.mitwatv.in/";
+/*
+    public static final String DOMAIN = "https://www.mitwatv.in/";
     public static final String Publisher_id = "40";
     public static final String WebsiteURL = "https://www.mitwatv.in/";*/
-/*
+
     public static final String DOMAIN = "https://ott.candorsolution.com/";
     public static final String Publisher_id = "61";
     public static final String WebsiteURL = "https://ott.candorsolution.com/";
-*/
 
 
 /*
@@ -77,7 +78,7 @@ public class Config {
 
     public static final String VideoURLTypeHls = "hls";
     public static final boolean LOGIN_REQUIRED = true;
-    public static final boolean NPAW = true;
+    public static final boolean NPAW = false;
     public static final String NPAW_APPNAME = "MitwaTV";
 
     public static final boolean CouponCodeEnable = false;
