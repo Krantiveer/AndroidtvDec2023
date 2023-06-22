c58c49e2553d9fb13d5444c0810daa278123e1f5
ubuntu <krantiveer.singh@phando.com>
2023-06-22 18:40:37 +0530

    om tv code generated

--- app/src/main/java/com/ott/tv/ui/activity/SplashScreenActivityTv.kt
+++ app/src/main/java/com/ott/tv/ui/activity/SplashScreenActivityTv.kt
@@ -43,7 +43,7 @@ class SplashScreenActivityTv : Activity() {
 
         }else {
             findViewById<LinearLayout>(R.id.splash_screen_ll).visibility=View.VISIBLE
-            if(BuildConfig.FLAVOR.equals("candor",ignoreCase = true)||BuildConfig.FLAVOR.equals("naaptolott")){
+            if(BuildConfig.FLAVOR.equals("candor",ignoreCase = true)||BuildConfig.FLAVOR.equals("naaptolott")||BuildConfig.FLAVOR.equals("omtvott")){
                 findViewById<ImageView>(R.id.splash_img_view).visibility=View.INVISIBLE
 
             }
